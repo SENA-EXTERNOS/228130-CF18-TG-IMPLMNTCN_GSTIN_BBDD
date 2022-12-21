e2c8641f8fcb4dfb4f005e96f8f92c967114a395
JPaolaMontilloG <paola.montillog@gmail.com>
2022-12-21 18:23:40 -0500

    Update Introduccion.vue
    + Se mejora la visualización en móviles.
    + Se agregan animaciones.

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -10,13 +10,13 @@
       h1 Introducción
 
     .row.justify-content-center.mb-5
-      .col-md-1.col-sm-10
+      .col-md-1.col-sm-2(data-aos="fade-right")
         figure
           img(src='@/assets/curso/temas/intro/intro.svg', alt="Imagen decorativa")
-      .col-md-11
+      .col-md-11.col-sm-10(data-aos="fade-left")
         p En el presente componente se desarrollan contenidos relacionados con la construcción del informe de requisitos de software que va de la mano con la recolección de información, pues no se puede priorizar una de la otra, simplemente se complementan, puesto que mientras una permite obtener la información necesaria para iniciar el proyecto, también nos permiten entender y construir todo el comportamiento del nuevo sistema que se planea desarrollar. Se invita a revisar el siguiente video para identificar el contexto de aprendizaje.
     
-    figure.mb-5
+    figure.mb-5(data-aos="zoom-in")
     .video
       iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
